5a7355ba4d842d8401c6d998694bb5482bbb6752
Fred Baube <fbaube@welho.com>
2020-08-17 20:11:17 +0300

    Minor changes.

--- createtables_sqlite.go
+++ createtables_sqlite.go
@@ -1,11 +1,11 @@
 package db
 
 import (
-  "fmt"
-  "io"
-  "os"
-  "database/sql"
-  S "strings"
+	"database/sql"
+	"fmt"
+	"io"
+	"os"
+	S "strings"
 )
 
 // CreateTable_sqlite creates a table for our simplified DB model where
@@ -14,7 +14,7 @@ import (
 func (pDB *MmmcDB) CreateTable_sqlite(ts TableSpec) {
 	var CTS string // the Create Table string
 	var hasFKs, hasInts, hasStrs bool
-	hasFKs  = (ts.forenKeys != nil && len(ts.forenKeys) > 0)
+	hasFKs = (ts.forenKeys != nil && len(ts.forenKeys) > 0)
 	hasInts = (ts.intFields != nil && len(ts.intFields) > 0)
 	hasStrs = (ts.strFields != nil && len(ts.strFields) > 0)
 
@@ -52,34 +52,45 @@ func (pDB *MmmcDB) CreateTable_sqlite(ts TableSpec) {
 		// FOREIGN KEY(idx_inb) REFERENCES INB(idx_inb)
 		for _, tbl := range ts.forenKeys {
 			// idx_inb integer not null references INB,
+			// TMP := "foreign key(idx_" + tbl + ") references " + tbl + "(idx_" + tbl + "), \n"
+			// println("TMP:", TMP)
 			CTS += "foreign key(idx_" + tbl + ") references " + tbl + "(idx_" + tbl + "), \n"
 		}
 	}
 	CTS = S.TrimSuffix(CTS, "\n")
 	CTS = S.TrimSuffix(CTS, " ")
 	CTS = S.TrimSuffix(CTS, ",")
-	CTS += "\n)"
+	CTS += "\n);"
 	println("= = = = = = = = \n", CTS, "= = = = = = = =")
 	pDB.DB.MustExec(CTS)
-  pDB.DumpTableSchema_sqlite(ts.tableName, os.Stdout)
+	pDB.DumpTableSchema_sqlite(ts.tableName, os.Stdout)
 	println("TODO: Insert record with IDX 0 and string descr's")
-  println("TODO: Dump all table records (i.e. just one)")
+	println("TODO: Dump all table records (i.e. just one)")
 }
 
 func (pDB *MmmcDB) DumpTableSchema_sqlite(tableName string, w io.Writer) {
-  println("TODO: Dump table structure:", tableName)
-  var R *sql.Rows
-  var e error
-  var C []*sql.ColumnType
-  // func (db *DB) Query(query string, args ...interface{}) (*Rows, error)
-  // func (rs *Rows) ColumnTypes() ([]*ColumnType, error)
-  R,e = pDB.DB.Query("select * from " + tableName)
-  if e != nil { panic("DB: sqlite1: " + e.Error())}
-  C,e = R.ColumnTypes()
-  if e != nil { panic("DB: sqlite2: " + e.Error())}
-  for i, c := range C {
-    L,_ := c.Length()
-    fmt.Printf("col[%d]: %s \t dbtp<%s> len<%d> gotp<%v> \n",
-      i, c.Name(), c.DatabaseTypeName(), L, c.ScanType())
-  }
+	println("TODO: Dump table structure:", tableName)
+	var R *sql.Rows
+	var e error
+	var CT []*sql.ColumnType
+	// func (db *DB) Query(query string, args ...interface{}) (*Rows, error)
+	// func (rs *Rows) ColumnTypes() ([]*ColumnType, error)
+	R, e = pDB.DB.Query("select * from " + tableName)
+	if e != nil {
+		panic("DB: sqlite1: " + e.Error())
+	}
+	CT, e = R.ColumnTypes()
+	if e != nil {
+		panic("DB: sqlite2: " + e.Error())
+	}
+	for i, ct := range CT {
+		L, _ := ct.Length()
+		st := ct.ScanType()
+		sst := ""
+		if st != nil {
+			sst = fmt.Sprintf("scantp<%+v> ", st)
+		}
+		fmt.Printf("col[%d]: %s \t dbtpnm<%s> len<%d> %s \n",
+			i, ct.Name(), ct.DatabaseTypeName(), L, sst)
+	}
 }
